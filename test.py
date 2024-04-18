46fddbb33c51bc7e12457f70b4d2c2e9d248a48f
umahar <umaharabdullah@@gmail.com>
2024-04-18 22:55:11 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+def adjective_to_verb(sentence, index):
+    """Change the adjective within the sentence to a verb.
+
+    :param sentence: str - that uses the word in sentence.
+    :param index: int - index of the word to remove and transform.
+    :return: str - word that changes the extracted adjective to a verb.
+
+    For example, ("It got dark as the sun set.", 2) becomes "darken".
+    """
+
+    newword=sentence.split()[index]
+    return newword[:-1]+'eh'
+
+
+print (adjective_to_verb('Her eyes were light blue.', -2 ))
